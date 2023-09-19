c4537db5e610d971250df95b1f25b594dede33ec
KC Kim <kc.kim@berkeley.edu>
2023-09-18 21:27:55 -0700

    Finished

--- src/datastructures/binarysearchtree/Main.java
+++ src/datastructures/binarysearchtree/Main.java
@@ -52,5 +52,28 @@ public class Main {
         System.out.println("\nRoot: " + myBST.root.value);
         System.out.println("\nRoot->Left: " + myBST.root.left.value);
         System.out.println("\nRoot->Right: " + myBST.root.right.value);
+
+        // Testing Breadth First Search
+        BinarySearchTree myBST2 = new BinarySearchTree();
+
+        myBST2.insert(47);
+        myBST2.insert(21);
+        myBST2.insert(76);
+        myBST2.insert(18);
+        myBST2.insert(27);
+        myBST2.insert(52);
+        myBST2.insert(82);
+
+        System.out.println("\nTesting BFS()");
+        System.out.println(myBST2.BFS());
+
+        System.out.println("\nTesting DFSPreOrder()");
+        System.out.println(myBST2.DFSPreOrder());
+
+        System.out.println("\nTesting DFSPostOrder()");
+        System.out.println(myBST2.DFSPostOrder());
+
+        System.out.println("\nTesting DFSInOrder()");
+        System.out.println(myBST2.DFSInOrder());
     }
 }
